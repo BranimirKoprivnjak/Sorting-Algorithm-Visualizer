728341205b07f683f6df0d3d098a185e7663d746
BranimirKoprivnjak <koprivnjak965@gmail.com>
2021-12-20 18:44:12 +0100

    tutorial added and name change

--- src/components/Chart/Legend.tsx
+++ src/components/Chart/Legend.tsx
@@ -8,49 +8,57 @@ import {
 
 const Legend: React.FC<{ algorithm: string; array: number[] }> = ({
   algorithm,
-  array,
 }) => {
   return (
-    <div
-      className={classes.legend}
-      style={{ visibility: algorithm ? 'visible' : 'hidden' }}
-    >
-      <div>
-        <h2>{algorithm}</h2>
-      </div>
-      <div className={classes.section}>
-        <div className={classes.container}>
-          <div
-            className={classes.color}
-            style={{ backgroundColor: `${SECONDARY_COLOR}` }}
-          ></div>
-          <p>Comparison</p>
+    <>
+      {!algorithm && (
+        <div className={classes.tutorial}>
+          Welcome to Sorting Algorithm Visualizer! A sorting algorithm is an
+          algorithm that puts elements of a list into an order. Each bar below
+          represents a random number between 5 and 800. Try out different Sorts
+          to visualize their unique ways of sorting the list!
         </div>
-        <div className={classes.container}>
-          <div
-            className={classes.color}
-            style={{ backgroundColor: `${TERTIARY_COLOR}` }}
-          ></div>
-          <p>{algorithm === 'Merge Sort' ? 'Change' : 'Swap'}</p>
-        </div>
-        {algorithm === 'Quick Sort' && (
-          <div className={classes.container}>
-            <div
-              className={classes.color}
-              style={{ backgroundColor: `${QUATERNARY_COLOR}` }}
-            ></div>
-            <p>Pivot</p>
+      )}
+      {algorithm && (
+        <div className={classes.legend}>
+          <div>
+            <h2>{algorithm}</h2>
           </div>
-        )}
-        <div className={classes.container}>
-          <div
-            className={classes.color}
-            style={{ backgroundColor: `${QUINARY_COLOR}` }}
-          ></div>
-          <p>Sorted</p>
+          <div className={classes.section}>
+            <div className={classes.container}>
+              <div
+                className={classes.color}
+                style={{ backgroundColor: `${SECONDARY_COLOR}` }}
+              ></div>
+              <p>Comparison</p>
+            </div>
+            <div className={classes.container}>
+              <div
+                className={classes.color}
+                style={{ backgroundColor: `${TERTIARY_COLOR}` }}
+              ></div>
+              <p>{algorithm === 'Merge Sort' ? 'Change' : 'Swap'}</p>
+            </div>
+            {algorithm === 'Quick Sort' && (
+              <div className={classes.container}>
+                <div
+                  className={classes.color}
+                  style={{ backgroundColor: `${QUATERNARY_COLOR}` }}
+                ></div>
+                <p>Pivot</p>
+              </div>
+            )}
+            <div className={classes.container}>
+              <div
+                className={classes.color}
+                style={{ backgroundColor: `${QUINARY_COLOR}` }}
+              ></div>
+              <p>Sorted</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
